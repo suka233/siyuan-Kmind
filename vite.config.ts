0ced47da21f90594e5be070e9a3c0782ef066aa5
宋子豪 <suka233@outlook.com>
2023-06-27 18:45:27 +0800

    ci: 更新打包方式

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,22 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import WindiCSS from 'vite-plugin-windicss';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+
+import minimist from 'minimist';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// import livereload from 'rollup-plugin-livereload';
+// import zipPack from 'vite-plugin-zip-pack';
+// import fg from 'fast-glob';
+
+const args = minimist(process.argv.slice(2));
+const isWatch = args.watch || args.w || false;
+const devDistDir = './dev';
+const distDir = isWatch ? devDistDir : './dist';
+
+console.log('isWatch=>', isWatch);
+console.log('distDir=>', distDir);
+
 function pathResolve(dir: string) {
     return resolve(process.cwd(), '.', dir);
 }
@@ -17,7 +33,31 @@ export default defineConfig({
             },
         ],
     },
-    plugins: [vue(), WindiCSS(), vueJsx()],
+    plugins: [
+        vue(),
+        WindiCSS(),
+        vueJsx(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: './README*.md',
+                    dest: './',
+                },
+                {
+                    src: './icon.png',
+                    dest: './',
+                },
+                {
+                    src: './preview.png',
+                    dest: './',
+                },
+                {
+                    src: './widget.json',
+                    dest: './',
+                },
+            ],
+        }),
+    ],
     server: {
         proxy: {
             '/api': {
@@ -28,14 +68,18 @@ export default defineConfig({
         },
     },
     build: {
-        rollupOptions: {
-            output: {
-                entryFileNames: 'js/[name].js',
-                chunkFileNames: 'js/[name].[hash].js',
-                assetFileNames: '[ext]/[name].[ext]',
-                dir: 'siyuan-Kmind',
-            },
-        },
+        // rollupOptions: {
+        //     output: {
+        //         entryFileNames: 'js/[name].js',
+        //         chunkFileNames: 'js/[name].[hash].js',
+        //         assetFileNames: '[ext]/[name].[ext]',
+        //         dir: 'siyuan-Kmind',
+        //     },
+        // },
+        outDir: distDir,
+        emptyOutDir: false,
+        sourcemap: false,
+        minify: !isWatch,
     },
     css: {
         preprocessorOptions: {
@@ -43,5 +87,8 @@ export default defineConfig({
                 javascriptEnabled: true,
             },
         },
+    },
+    define: {
+        'process.env.DEV_MODE': `"${isWatch}"`,
     },
 });
